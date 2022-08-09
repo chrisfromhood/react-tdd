33aa4b4cc173677a38c881670589597ce20b0ae2
chrisfromhood <kristijan@cogentdeciphering.com>
2022-08-09 12:00:06 +0200

    s03

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import axios from 'axios';
+import Input from '../components/Input';
 
 class SignUpPage extends Component {
   state = {
@@ -8,13 +9,17 @@ class SignUpPage extends Component {
     password: '',
     passwordRepeat: '',
     apiProgress: false,
-    signUpSuccess: false
+    signUpSuccess: false,
+    errors: {}
   };
 
   onChange = (event) => {
     const { id, value } = event.target;
+    const errorsCopy = { ...this.state.errors };
+    delete errorsCopy[id];
     this.setState({
-      [id]: value
+      [id]: value,
+      errors: errorsCopy
     });
   };
 
@@ -30,15 +35,25 @@ class SignUpPage extends Component {
     try {
       await axios.post('/api/1.0/users', body);
       this.setState({ signUpSuccess: true });
-    } catch (error) { }
+    } catch (error) {
+      if (error.response.status === 400) {
+        this.setState({ errors: error.response.data.validationErrors });
+      }
+      this.setState({ apiProgress: false });
+    }
   };
 
   render() {
     let disabled = true;
-    const { password, passwordRepeat, apiProgress, signUpSuccess } = this.state;
+    const { password, passwordRepeat, apiProgress, signUpSuccess, errors } =
+      this.state;
     if (password && passwordRepeat) {
       disabled = password !== passwordRepeat;
     }
+
+    let passwordMismatch =
+      password !== passwordRepeat ? 'Password mismatch' : '';
+
     return (
       <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
         {!signUpSuccess && (
@@ -47,48 +62,32 @@ class SignUpPage extends Component {
               <h1 className="text-center">Sign Up</h1>
             </div>
             <div className="card-body">
-              <div className="mb-3">
-                <label htmlFor="username" className="form-label">
-                  Username
-                </label>
-                <input
-                  id="username"
-                  className="form-control"
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label className="form-label" htmlFor="email">
-                  E-mail
-                </label>
-                <input
-                  className="form-control"
-                  id="email"
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label className="form-label" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  className="form-control"
-                  id="password"
-                  type="password"
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label className="form-label" htmlFor="passwordRepeat">
-                  Password Repeat
-                </label>
-                <input
-                  className="form-control"
-                  id="passwordRepeat"
-                  type="password"
-                  onChange={this.onChange}
-                />
-              </div>
+              <Input
+                id="username"
+                label="Username"
+                onChange={this.onChange}
+                help={errors.username}
+              />
+              <Input
+                id="email"
+                label="E-mail"
+                onChange={this.onChange}
+                help={errors.email}
+              />
+              <Input
+                id="password"
+                label="Password"
+                onChange={this.onChange}
+                help={errors.password}
+                type="password"
+              />
+              <Input
+                id="passwordRepeat"
+                label="Password Repeat"
+                onChange={this.onChange}
+                help={passwordMismatch}
+                type="password"
+              />
               <div className="text-center">
                 <button
                   className="btn btn-primary"
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -65,20 +65,21 @@ describe('Sign Up Page', () => {
 
     beforeEach(() => {
       counter = 0;
+      server.resetHandlers();
     });
 
     beforeAll(() => server.listen());
 
     afterAll(() => server.close());
 
-    let button;
+    let button, usernameInput, emailInput, passwordInput, passwordRepeatInput;
 
     const setup = () => {
       render(<SignUpPage />);
-      const usernameInput = screen.getByLabelText('Username');
-      const emailInput = screen.getByLabelText('E-mail');
-      const passwordInput = screen.getByLabelText('Password');
-      const passwordRepeatInput = screen.getByLabelText('Password Repeat');
+      usernameInput = screen.getByLabelText('Username');
+      emailInput = screen.getByLabelText('E-mail');
+      passwordInput = screen.getByLabelText('Password');
+      passwordRepeatInput = screen.getByLabelText('Password Repeat');
       userEvent.type(usernameInput, 'user1');
       userEvent.type(emailInput, 'user1@mail.com');
       userEvent.type(passwordInput, 'P4ssword');
@@ -139,5 +140,63 @@ describe('Sign Up Page', () => {
         expect(form).not.toBeInTheDocument();
       });
     });
+
+    const generateValidationError = (field, message) => {
+      return rest.post('/api/1.0/users', (req, res, ctx) => {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            validationErrors: { [field]: message }
+          })
+        );
+      });
+    };
+
+    it.each`
+      field         | message
+      ${'username'} | ${'Username cannot be null'}
+      ${'email'}    | ${'E-mail cannot be null'}
+      ${'password'} | ${'Password cannot be null'}
+    `('displays $message for $field', async ({ field, message }) => {
+      server.use(generateValidationError(field, message));
+      setup();
+      userEvent.click(button);
+      const validationError = await screen.findByText(message);
+      expect(validationError).toBeInTheDocument();
+    });
+
+    it('hides spinner and enables button after response received', async () => {
+      server.use(
+        generateValidationError('username', 'Username cannot be null')
+      );
+      setup();
+      userEvent.click(button);
+      await screen.findByText('Username cannot be null');
+      expect(screen.queryByRole('status')).not.toBeInTheDocument();
+      expect(button).toBeEnabled();
+    });
+    it('displays mismatch message for password repeat input', () => {
+      setup();
+      userEvent.type(passwordInput, 'P4ssword');
+      userEvent.type(passwordRepeatInput, 'AnotherP4ssword');
+      const validationError = screen.queryByText('Password mismatch');
+      expect(validationError).toBeInTheDocument();
+    });
+    it.each`
+      field         | message                      | label
+      ${'username'} | ${'Username cannot be null'} | ${'Username'}
+      ${'email'}    | ${'E-mail cannot be null'}   | ${'E-mail'}
+      ${'password'} | ${'Password cannot be null'} | ${'Password'}
+    `(
+      'clears validation error after $field is updated',
+      async ({ field, message, label }) => {
+        server.use(generateValidationError(field, message));
+        setup();
+        userEvent.click(button);
+        const validationError = await screen.findByText(message);
+        userEvent.type(screen.getByLabelText(label), 'updated');
+        expect(validationError).not.toBeInTheDocument();
+      }
+    );
   });
 });
